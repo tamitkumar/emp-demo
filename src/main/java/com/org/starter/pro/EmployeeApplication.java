d57947fe7aa49595867d256176073527ec80b782
tamitkumar <60092266+tamitkumar@users.noreply.github.com>
2024-07-08 15:31:57 +0530

    Update EmployeeApplication.java

--- src/main/java/com/org/starter/pro/EmployeeApplication.java
+++ src/main/java/com/org/starter/pro/EmployeeApplication.java
@@ -16,7 +16,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class EmployeeApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
-
 	}
 	
 	@Override
